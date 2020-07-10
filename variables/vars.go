b9474c4b2f1c955b35fefa8c13d91f34f4323b5f
mostafa H <mostafaheidary@hotmail.com>
2020-07-10 17:38:22 +0430

    feat: 🎸 add more hints

--- variables/vars.go
+++ variables/vars.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // we can't use := in the package level.
-var i int = 42
+var i int = 27
 
 // var block
 var (
@@ -12,9 +15,21 @@ var (
 )
 
 func main() {
+	fmt.Println("Program started ... let's talk about variables.")
+	fmt.Println(i)
 	var i int = 42
 	fmt.Println(i)
 
 	j := 42
-	fmt.Printf("%v, %T", j, j)
+	fmt.Printf("%v, %T\n", j, j)
+
+	var k float32
+	k = float32(j)
+	fmt.Printf("%v, %T\n", k, k)
+
+	var l string
+	l = string(i)
+	fmt.Printf("%v, %T\n", l, l)
+	l = strconv.Itoa(i)
+	fmt.Printf("%v, %T", l, l)
 }
